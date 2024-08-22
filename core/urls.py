013b0fdfeb211be2dee37a35622a050732d92b5d
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-08-22 14:25:10 +0600

    compose

--- core/urls.py
+++ core/urls.py
@@ -18,11 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
     path('cms/admin/', admin.site.urls),
-    path('', include('cms.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('cms.urls')),
+    prefix_default_language=False
+)
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
